5b86bed979d8fd5019e6917d1c7349a982cfd430
Muhammad Dawood Rafique <mdawoodrafique394@gmail.com>
2025-05-06 16:19:45 +0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -24,6 +24,7 @@ export default function Home() {
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
+          <li className="tracking-[-.01em]">Its the Updated version.....</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
